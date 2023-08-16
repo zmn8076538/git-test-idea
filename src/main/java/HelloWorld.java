0fcdf23ad685da35c6c25cb70431f54ef8922910
zmn <zmn@sina.com>
2023-08-16 14:22:22 +0800

    加了一行hello world

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
         System.out.println("Hello Git");
         System.out.println("Hello Git");
         System.out.println("Hello Git");
+        System.out.println("Hello World");
     }
 }
