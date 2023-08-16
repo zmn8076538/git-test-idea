6444fb86e39a6ed4114178d7b96ce8b6af75222d
zmn <zmn@sina.com>
2023-08-16 14:30:25 +0800

    master分支加了第9行

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
         System.out.println("Hello Git");
         System.out.println("Hello Git");
         System.out.println("Hello World");
+        System.out.println("Hello Master");
     }
 }
